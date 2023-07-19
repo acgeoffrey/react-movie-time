61700cecb74677b2201607147d4c13468f3ebe5c
Geoffrey <gijo@Geoffreys-MacBook-Air.local>
2023-07-19 14:30:07 +0530

    created custom hooks for all actions

---
+++ src/components/App.js
@@ -0,0 +1,91 @@
+import { useEffect, useState } from "react";
+import Loader from "./Loader";
+import {
+  WatchedMovieList,
+  WatchedSummary,
+  MovieDetails,
+  MovieList,
+  Box,
+  Main,
+  ErrorMessage,
+  Navbar,
+  Logo,
+  Search,
+  NumResults,
+} from "./";
+import { useMovies } from "../hooks/useMovies";
+import { useLocalStorageState } from "../hooks/useLocalStorageState";
+
+export const average = (arr) =>
+  arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
+
+export default function App() {
+  const [query, setQuery] = useState("");
+  const [selectedId, setSelectedId] = useState(null);
+  const { movies, error, isLoading } = useMovies(query);
+  const [watched, setWatched] = useLocalStorageState([], "watched");
+
+  function handleSelectMovie(id) {
+    setSelectedId((selectedId) => (id === selectedId ? null : id));
+  }
+
+  function handleCloseMovie() {
+    setSelectedId(null);
+  }
+
+  function handleAddWatched(movie) {
+    setWatched((watched) => [...watched, movie]);
+  }
+
+  function handleDeleteWatched(id) {
+    setWatched((watched) => watched.filter((movie) => movie.imdbId !== id));
+  }
+
+  return (
+    <>
+      <Navbar>
+        <Logo />
+        <Search query={query} setQuery={setQuery} />
+        <NumResults movies={movies} />
+      </Navbar>
+      <Main>
+        {/* <Box element={<MovieList movies={movies} />} />
+        <Box
+          element={
+            <>
+              <WatchedSummary watched={watched} />
+              <WatchedMovieList watched={watched} />
+            </>
+          }
+        /> */}
+        <Box>
+          {/* {isLoading ? <Loader /> : <MovieList movies={movies} />} */}
+          {isLoading && <Loader />}
+          {!isLoading && !error && (
+            <MovieList movies={movies} onSelectMovie={handleSelectMovie} />
+          )}
+          {error && <ErrorMessage message={error} />}
+        </Box>
+
+        <Box>
+          {selectedId ? (
+            <MovieDetails
+              selectedId={selectedId}
+              onCloseMovie={handleCloseMovie}
+              onAddWatched={handleAddWatched}
+              watched={watched}
+            />
+          ) : (
+            <>
+              <WatchedSummary watched={watched} />
+              <WatchedMovieList
+                watched={watched}
+                onDeleteWatched={handleDeleteWatched}
+              />
+            </>
+          )}
+        </Box>
+      </Main>
+    </>
+  );
+}
---
+++ src/components/index.js
@@ -0,0 +1,25 @@
+import { WatchedSummary } from "./WatchedSummary";
+import { WatchedMovieList } from "./WatchedMovieList";
+import { MovieDetails } from "./MovieDetails";
+import { MovieList } from "./MovieList";
+import { Box } from "./Box";
+import { Main } from "./Main";
+import { ErrorMessage } from "./ErrorMessage";
+import { Navbar } from "./Navbar";
+import { Logo } from "./Logo";
+import { Search } from "./Search";
+import { NumResults } from "./NumResults";
+
+export {
+  WatchedMovieList,
+  WatchedSummary,
+  MovieDetails,
+  MovieList,
+  Box,
+  Main,
+  ErrorMessage,
+  Navbar,
+  Logo,
+  Search,
+  NumResults,
+};
